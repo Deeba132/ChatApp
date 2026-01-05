30229f55df129291d0d6e85147e1133f5fcb5833
Deeba132 <deebashabreen@gmail.com>
2026-01-05 11:48:59 +0530

    added Stomp Registry in connectionconfig

--- chats/demo/src/main/java/com/example/demo/ConnectionConfig.java
+++ chats/demo/src/main/java/com/example/demo/ConnectionConfig.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
@@ -12,5 +13,10 @@ public class ConnectionConfig implements WebSocketMessageBrokerConfigurer{
     public void configureMessageBroker(MessageBrokerRegistry config){
           config.enableSimpleBroker("/topics");//server to client at "/"
           config.setApplicationDestinationPrefixes("/app"); //client to application controller
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry.addEndpoint("/ws").withSockJS();
     }
 }
