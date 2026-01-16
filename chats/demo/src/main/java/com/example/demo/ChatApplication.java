ca0947f904864c92bbb5f02c858879222282c3c7
Deeba132 <deebashabreen@gmail.com>
2026-01-16 23:29:20 +0530

    Added spring dotenv dependency such that env vars are injected

--- chats/demo/src/main/java/com/example/demo/ChatApplication.java
+++ chats/demo/src/main/java/com/example/demo/ChatApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @ServletComponentScan
 public class ChatApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry ->
+			System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(ChatApplication.class, args);
 	}
 
